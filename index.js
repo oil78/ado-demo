76c7d9bbdda668cda289c8744c514b14180796a4
oil78 <122600981+oil78@users.noreply.github.com>
2026-03-19 15:32:52 +0000

    Set up HTTP server in index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Pipeline triggered again x 2!");
+const http = require("http");
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello from Azure Web App!");
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
